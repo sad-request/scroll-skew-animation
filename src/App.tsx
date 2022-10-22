234e6de83c74285d6f2c8a31e5765c09691d0383
https://github.com/IbatullinaMaria <mpbugaga@yandex.ru>
2022-10-22 18:54:36 +0300

    delete package-lock

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, odit. Ratione deleniti
+          similique eveniet voluptates repellat fugit possimus voluptatem impedit officia sunt,
+          omnis velit beatae nihil aliquam illo vel ipsum.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, odit. Ratione deleniti
+          similique eveniet voluptates repellat fugit possimus voluptatem impedit officia sunt,
+          omnis velit beatae nihil aliquam illo vel ipsum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, odit. Ratione deleniti
+          similique eveniet voluptates repellat fugit possimus voluptatem impedit officia sunt,
+          omnis velit beatae nihil aliquam illo vel ipsum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, odit. Ratione deleniti
+          similique eveniet voluptates repellat fugit possimus voluptatem impedit officia sunt,
+          omnis velit beatae nihil aliquam illo vel ipsum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, odit. Ratione deleniti
+          similique eveniet voluptates repellat fugit possimus voluptatem impedit officia sunt,
+          omnis velit beatae nihil aliquam illo vel ipsum.
+        </p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
